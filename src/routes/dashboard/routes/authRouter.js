38e4b857adaa9c8b1e8a596ee315820b670440ec
Fajarudeen <110659059+Fajarudeen@users.noreply.github.com>
2024-03-19 10:21:11 +0530

    Push

---
+++ src/routes/dashboard/routes/authRouter.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { postAuthentication, validateToken } from '../../../controllers/dashboard/authController.js';
+import { authMiddleware } from '../../../middleware/authMiddleware.js';
+
+export const authenticationRouter = express.Router();
+
+authenticationRouter.post('/login', postAuthentication);
+
+authenticationRouter.get('/validate', authMiddleware, validateToken);
